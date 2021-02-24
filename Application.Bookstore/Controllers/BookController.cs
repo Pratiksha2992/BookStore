0b40913f2f81ee3d9a92a20c96aad3453198d6df
dhavalepratiksha9240 <dhavalepratiksha9240@gmail.com>
2021-02-24 21:16:15 +0530

    Without Database

---
+++ Application.Bookstore/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Bookstore.Models;
+using Application.Bookstore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository __bookRepository = null;
+        public BookController()
+        {
+            __bookRepository = new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return __bookRepository.GetAllBooks();
+        }
+        public BookModel GetBook(int id)
+        {
+            return __bookRepository.GetBookById(id);
+        }
+        public List<BookModel> SearchBooks(string bookName,string authorName)
+        {
+            return __bookRepository.SearchBook(bookName,authorName);
+        }
+    }
+}
